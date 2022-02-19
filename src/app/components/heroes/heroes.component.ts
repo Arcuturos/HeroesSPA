941482c174d06de09fd8f6cdd01cbaa2a0867a2d
unknown <Arturodorantes126@gmail.com>
2022-02-19 09:43:59 -0600

    Initial commit

---
+++ src/app/components/heroes/heroes.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { HeroesService} from '../../servicios/heroes.service'
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+heroes:Heroe[] = [];
+
+  constructor(private _heroesService:HeroesService,
+    private router:Router) { }
+
+  ngOnInit(): void {
+
+    this.heroes = this._heroesService.getHeroes();
+
+    // console.log(this.heroes);
+  }
+  verHeroe(idx:number){
+    this.router.navigate(['/heroe',idx])
+  }
+  }
+
+export interface Heroe{
+
+nombre: string;
+bio: string;
+img: string;
+aparicion: string;
+casa: string;
+
+ };
+
+
